4de1cd52b9880c439c168bd303f3e518fbd8a459
Regina Battle <hello@reginabattle.com>
2018-05-19 16:45:25 -0400

    Create a profile module and reorganize code

---
+++ profile.js
@@ -0,0 +1,69 @@
+// Connect to Treehouse's API to get a user's badge count and JavaScript points.
+
+const https = require('https');
+const http = require('http');
+
+// Print error messages
+function printError(error){
+	console.error(error.message);
+}
+
+// Print badge count and points
+function print(username, badgeCount, points){
+	const message = `${username} has ${badgeCount} badges and ${points} JavaScript points.`;
+	console.log(message);
+}
+
+// Get profile info
+function get(username) {
+	try {
+		// Connect to the API
+		const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+
+			// Get status
+			const status = response.statusCode;
+
+			if(status === 200) {
+				// Get data
+				let body = "";
+				response.on('data', data =>{
+					body += data.toString();
+					//console.log(body); Print out JSON file
+				});
+
+				// Convert string (data) to an object
+				response.on('end', () =>{
+					try {
+						const profile = JSON.parse(body);
+						//console.log(profile); Print out parsed JSON file
+
+						// Print out the data
+						print(profile.name, profile.badges.length, profile.points['JavaScript']);
+
+					} catch(error) {
+						printError(error);
+					}
+				});
+
+			} else {
+				const message = `There was a problem  getting ${username}'s profile (${http.STATUS_CODES[response.statusCode]}).`;
+				const statusCodeError = new Error(message);
+				printError(statusCodeError);
+			}
+		});
+
+		request.on('error', printError);
+
+	} catch(error) {
+		printError(error);
+	}
+};
+
+// Make module accessible
+module.exports.get = get;
+
+// Get info for a single user
+//getProfile("reginabattle");
+
+// Get info for multiple users
+//const users = ["reginabattle", "chalkers"];
