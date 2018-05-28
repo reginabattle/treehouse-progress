61e96ce20b9e8c88739c28d1b06fc4cedef27ee5
Regina Battle <hello@reginabattle.com>
2018-05-28 13:44:20 -0400

    Print out basic info and fix profile module path

--- config/router.js
+++ config/router.js
@@ -1,4 +1,6 @@
 
+const Profile = require("../profile/profile.js");
+
 // Home Route
 function home(request, response) {
 	if(request.url === "/"){
@@ -9,14 +11,35 @@ function home(request, response) {
 	}
 }
 
-// User Router
+// User Route
 function user(request, response) {
 	const username = request.url.replace("/", "");
 	if(username.length > 0){
 		response.writeHead(200, {'Content-type':'text/plain'});
 		response.write(`Header \n`);
-		response.write(`${username} \n`);
-		response.end(`Footer \n`);
+
+		// Get JSON from Treehouse
+		const studentProfile = new Profile(username);
+		studentProfile.on("end", profileJSON => {
+
+			// Store profile info
+			const values = {
+				avatar: profileJSON.gravatar_url,
+				name: profileJSON.name,
+				username: profileJSON.profile_name,
+				badges: profileJSON.badges.length,
+				javascriptPoints: profileJSON.points['JavaScript']
+			}
+
+			// Response
+			response.write(`${values.name} has ${values.badges} badges. \n`);
+			response.end(`Footer \n`);
+		});
+
+		// Error
+		studentProfile.on("error", error => {
+			response.end(`Footer \n`);
+		});
 	}
 }
 
